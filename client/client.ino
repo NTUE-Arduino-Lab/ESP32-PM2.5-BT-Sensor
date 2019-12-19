97a5d4ca7293cf0cde0d1e4fda91c9c720f04930
e2ne0 <e2ne00@gmail.com>
2019-12-19 21:55:50 +0800

    use for pm2.5

--- client/client.ino
+++ client/client.ino
@@ -1,165 +1,204 @@
+#include <Adafruit_SH1106.h>
+#include <Adafruit_GFX.h>
 #include <BLEDevice.h>
-#include <Wire.h>                   // For I2C interface
+#include <Wire.h> // For I2C interface
 #include <Arduino.h>
+#include <SPI.h>
 
 /* 基本属性定義  */
-#define SPI_SPEED   115200          // SPI通信速度
+#define SPI_SPEED 115200 // SPI通信速度
 
 /* シグナル種別 */
-#define SIGNAL_ERROR   'E'          // (Error:異常発生)
+#define SIGNAL_ERROR 'E' // (Error:異常発生)
 
+#define OLED_SDA 22
+#define OLED_SCL 23
+
+Adafruit_SH1106 display(OLED_SDA, OLED_SCL);
 
 /* UUID定義 */
 BLEUUID serviceUUID("28b0883b-7ec3-4b46-8f64-8559ae036e4e");   // サービスのUUID
 BLEUUID CHARA_UUID_RX("2049779d-88a9-403a-9c59-c7df79e1dd7c"); // RXのUUID
 
 /* 通信制御用 */
-BLERemoteCharacteristic* pRemoteCharacteristicRX;  // 受信用キャラクタリスティック
-BLEAdvertisedDevice* targetDevice;      // 目的のBLEデバイス
-bool  doConnect = false;                // 接続指示
-bool  doScan = false;                   // スキャン指示
-bool  deviceConnected = false;          // デバイスの接続状態
-bool  bInAlarm  = false;                // デバイス異常
-bool  enableMeasurement = false;        // 計測情報が有効
+BLERemoteCharacteristic *pRemoteCharacteristicRX; // 受信用キャラクタリスティック
+BLEAdvertisedDevice *targetDevice;				  // 目的のBLEデバイス
+bool doConnect = false;							  // 接続指示
+bool doScan = false;							  // スキャン指示
+bool deviceConnected = false;					  // デバイスの接続状態
+bool bInAlarm = false;							  // デバイス異常
+bool enableMeasurement = false;					  // 計測情報が有効
 
 /* 通信データ */
-struct tmpData {                        // 計測データ
-    int   buttonState;
+struct tmpData
+{ // 計測データ
+	double pmData;
 };
-struct tmpData  data;
+struct tmpData data;
 
 /* LEDピン */
-const int ledPin = 16;                  // 接続ピン
+const int ledPin = 16; // 接続ピン
 
 /*********************< Callback classes and functions >**********************/
 // 接続・切断時コールバック
-class funcClientCallbacks: public BLEClientCallbacks {
-    void onConnect(BLEClient* pClient) {
-    };
-    void onDisconnect(BLEClient* pClient) {
-        deviceConnected = false;
-    }
+class funcClientCallbacks : public BLEClientCallbacks
+{
+	void onConnect(BLEClient *pClient){};
+	void onDisconnect(BLEClient *pClient)
+	{
+		deviceConnected = false;
+	}
 };
 
 // アドバタイジング受信時コールバック
-class advertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
-    void onResult(BLEAdvertisedDevice advertisedDevice) {
-        Serial.print("Advertised Device found: ");
-        Serial.println(advertisedDevice.toString().c_str());
+class advertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
+{
+	void onResult(BLEAdvertisedDevice advertisedDevice)
+	{
+		Serial.print("Advertised Device found: ");
+		Serial.println(advertisedDevice.toString().c_str());
 
-        // 目的のBLEデバイスならスキャンを停止して接続準備をする
-        if (advertisedDevice.haveServiceUUID()) {
-            BLEUUID service = advertisedDevice.getServiceUUID();
-            Serial.print("Have ServiceUUI: "); Serial.println(service.toString().c_str());
-            if (service.equals(serviceUUID)) {
-                BLEDevice::getScan()->stop();
-                targetDevice = new BLEAdvertisedDevice(advertisedDevice);
-                doConnect = doScan = true;
-            }
-        }
-    }
+		// 目的のBLEデバイスならスキャンを停止して接続準備をする
+		if (advertisedDevice.haveServiceUUID())
+		{
+			BLEUUID service = advertisedDevice.getServiceUUID();
+			Serial.print("Have ServiceUUI: ");
+			Serial.println(service.toString().c_str());
+			if (service.equals(serviceUUID))
+			{
+				BLEDevice::getScan()->stop();
+				targetDevice = new BLEAdvertisedDevice(advertisedDevice);
+				doConnect = doScan = true;
+			}
+		}
+	}
 };
 
 // Notify時のコールバック関数
-static void notifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic,
-                uint8_t* pData, size_t length, bool isNotify) {
+static void notifyCallback(BLERemoteCharacteristic *pBLERemoteCharacteristic,
+						   uint8_t *pData, size_t length, bool isNotify)
+{
 
-    // 受信メッセージからボタンを切り出して表示用に編集する
-    memcpy(&data, pData, length);
-    int c = data.buttonState;
-    if(c == 0)
-        digitalWrite(ledPin, HIGH);
-    else
-        digitalWrite(ledPin, LOW);    
-    enableMeasurement = true;
-    Serial.print("Received data: ");  Serial.print(c);
+	// 受信メッセージからボタンを切り出して表示用に編集する
+	memcpy(&data, pData, length);
+	int c = (int)data.pmData;
+	enableMeasurement = true;
+	Serial.print("Received data: ");
+	Serial.println(c);
 }
 
 /*****************************************************************************
  *                          Predetermined Sequence                           *
  *****************************************************************************/
-void setup() {
-    // 初期化処理を行ってBLEデバイスを初期化する
-    doInitialize();
-    BLEDevice::init("");
-    Serial.println("Client application start...");
+void setup()
+{
+	// 初期化処理を行ってBLEデバイスを初期化する
+	doInitialize();
+	BLEDevice::init("");
+	Serial.println("Client application start...");
 
-    // Scanオブジェクトを取得してコールバックを設定する
-    BLEScan* pBLEScan = BLEDevice::getScan();
-    pBLEScan->setAdvertisedDeviceCallbacks(new advertisedDeviceCallbacks());
-    // アクティブスキャンで10秒間スキャンする
-    pBLEScan->setActiveScan(true);
-    pBLEScan->start(10);
+	// Scanオブジェクトを取得してコールバックを設定する
+	BLEScan *pBLEScan = BLEDevice::getScan();
+	pBLEScan->setAdvertisedDeviceCallbacks(new advertisedDeviceCallbacks());
+	// アクティブスキャンで10秒間スキャンする
+	pBLEScan->setActiveScan(true);
+	pBLEScan->start(10);
 }
 
-void loop() {
-    // アドバタイジング受信時に一回だけサーバーに接続する
-    if (doConnect == true) {
-        if (doPrepare()) {
-            Serial.println("Connected to the BLE Server.");
-        } else {
-            Serial.println("Failed to connect to the BLE server.");
-        }
-        doConnect = false;
-    }
-    // 接続状態なら
-    if (deviceConnected) {
-        // 測定値が有効かつ異常でなければOLEDに表示する
-        if (enableMeasurement && !bInAlarm) {
-            
-            enableMeasurement = false;
-        }
-    } else if (doScan) {
-        BLEDevice::getScan()->start(0);
-    }
+void loop()
+{
+	// アドバタイジング受信時に一回だけサーバーに接続する
+	if (doConnect == true)
+	{
+		if (doPrepare())
+		{
+			Serial.println("Connected to the BLE Server.");
+		}
+		else
+		{
+			Serial.println("Failed to connect to the BLE server.");
+		}
+		doConnect = false;
+	}
+	// 接続状態なら
+	if (deviceConnected)
+	{
+		// 測定値が有効かつ異常でなければOLEDに表示する
+		if (enableMeasurement && !bInAlarm)
+		{
+
+			enableMeasurement = false;
+		}
+	}
+	else if (doScan)
+	{
+		BLEDevice::getScan()->start(0);
+	}
+	display.setTextSize(2);
+	display.setTextColor(WHITE);
+	display.setCursor(0, 0);
+	display.println("CURRENT");
+	display.println("PM2.5 :");
+	display.print(data.pmData);
+	display.println(" ug/m^3");
+	display.display();
+	delay(2000);
+	display.clearDisplay();
 }
 
 /*  初期化処理  */
-void doInitialize() {
-    Serial.begin(SPI_SPEED);
-    pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, HIGH);
+void doInitialize()
+{
+	Serial.begin(SPI_SPEED);
+	pinMode(ledPin, OUTPUT);
+	digitalWrite(ledPin, HIGH);
 
-    Serial.println("BLE Client start ...");
+	Serial.println("BLE Client start ...");
+	display.begin(SH1106_SWITCHCAPVCC, 0x3C); 
+  	display.clearDisplay();
 }
 
 /*  準備処理  */
-bool doPrepare() {
-    // クライアントオブジェクトを作成してコールバックを設定する
-    BLEClient* pClient = BLEDevice::createClient();
-    pClient->setClientCallbacks(new funcClientCallbacks());
-    Serial.println(" - Created client.");
+bool doPrepare()
+{
+	// クライアントオブジェクトを作成してコールバックを設定する
+	BLEClient *pClient = BLEDevice::createClient();
+	pClient->setClientCallbacks(new funcClientCallbacks());
+	Serial.println(" - Created client.");
 
-    // リモートBLEサーバーと接続して
-    pClient->connect(targetDevice);
-    Serial.println(" - Connected to server.");
+	// リモートBLEサーバーと接続して
+	pClient->connect(targetDevice);
+	Serial.println(" - Connected to server.");
 
-    // サービスへの参照を取得する
-    BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
-    if (pRemoteService == nullptr) {
-        Serial.print("Failed to find serviceUUID: ");
-        Serial.println(serviceUUID.toString().c_str());
-        pClient->disconnect();
-        return false;
-    }
-    Serial.println(" - Found target service.");
+	// サービスへの参照を取得する
+	BLERemoteService *pRemoteService = pClient->getService(serviceUUID);
+	if (pRemoteService == nullptr)
+	{
+		Serial.print("Failed to find serviceUUID: ");
+		Serial.println(serviceUUID.toString().c_str());
+		pClient->disconnect();
+		return false;
+	}
+	Serial.println(" - Found target service.");
 
-    // キャラクタリスティックへの参照を取得して
-    pRemoteCharacteristicRX = pRemoteService->getCharacteristic(CHARA_UUID_RX);
-    if (pRemoteCharacteristicRX == nullptr) {
-      Serial.print("Failed to find characteristicUUID: ");
-      Serial.println(CHARA_UUID_RX.toString().c_str());
-      pClient->disconnect();
-      return false;
-    }
-    Serial.println(" - Found characteristic CHARA_UUID_RX.");
+	// キャラクタリスティックへの参照を取得して
+	pRemoteCharacteristicRX = pRemoteService->getCharacteristic(CHARA_UUID_RX);
+	if (pRemoteCharacteristicRX == nullptr)
+	{
+		Serial.print("Failed to find characteristicUUID: ");
+		Serial.println(CHARA_UUID_RX.toString().c_str());
+		pClient->disconnect();
+		return false;
+	}
+	Serial.println(" - Found characteristic CHARA_UUID_RX.");
 
-    // Notifyのコールバック関数を割り当てる
-    if (pRemoteCharacteristicRX->canNotify()) {
-        pRemoteCharacteristicRX->registerForNotify(notifyCallback);
-        Serial.println(" - Registered notify callback function.");
-    }
+	// Notifyのコールバック関数を割り当てる
+	if (pRemoteCharacteristicRX->canNotify())
+	{
+		pRemoteCharacteristicRX->registerForNotify(notifyCallback);
+		Serial.println(" - Registered notify callback function.");
+	}
 
-    deviceConnected = true;
-    return true;
+	deviceConnected = true;
+	return true;
 }
--- server/server.ino
+++ server/server.ino
@@ -1,136 +1,203 @@
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLE2902.h>
-#include <Ticker.h>                 // タイマー割り込み用
+#include <Ticker.h> // タイマー割り込み用
 #include <Arduino.h>
 
 /* 基本属性定義  */
-#define DEVICE_NAME "ESP32"         // デバイス名
-#define SPI_SPEED   115200          // SPI通信速度
+#define DEVICE_NAME "ESP32" // デバイス名
+#define SPI_SPEED 115200	// SPI通信速度
 
 /* シグナル種別 */
-#define SIGNAL_ERROR   'E'          // (Error:異常発生)
+#define SIGNAL_ERROR 'E' // (Error:異常発生)
 
 /* UUID定義 */
-#define SERVICE_UUID           "28b0883b-7ec3-4b46-8f64-8559ae036e4e"  // サービスのUUID
-#define CHARACTERISTIC_UUID_TX "2049779d-88a9-403a-9c59-c7df79e1dd7c"  // 送信用のUUID
+#define SERVICE_UUID "28b0883b-7ec3-4b46-8f64-8559ae036e4e"			  // サービスのUUID
+#define CHARACTERISTIC_UUID_TX "2049779d-88a9-403a-9c59-c7df79e1dd7c" // 送信用のUUID
 
 /* 通信制御用 */
-BLECharacteristic *pCharacteristicTX;   // 送信用キャラクタリスティック
-bool  deviceConnected = false;          // デバイスの接続状態
-bool  bAbnormal  = false;               // デバイス異常判定
+BLECharacteristic *pCharacteristicTX; // 送信用キャラクタリスティック
+bool deviceConnected = false;		  // デバイスの接続状態
+bool bAbnormal = false;				  // デバイス異常判定
 
 /* 通信データ */
-struct tmpData {                        // 計測データ
-    int   buttonState;
+struct tmpData
+{ // 計測データ
+	double pmData;
 };
-struct tmpData  data;
-
-struct tmpSignal {                      // シグナル
-    char  hdr1;
-    char  signalCode;
+struct tmpData data;
+int incomeByte[7];
+int sensorData;
+int z = 0;
+int sum;
+struct tmpSignal
+{ // シグナル
+	char hdr1;
+	char signalCode;
 };
-struct tmpSignal signaldata = { 0xff, 0x00 };
+struct tmpSignal signaldata = {0xff, 0x00};
 
 /* LEDピン */
-const int ledPin = 16;                  // 接続ピン
-int ledState = LOW;                     // 状態
+// const int ledPin = 16; // 接続ピン
+// int ledState = LOW;	// 状態
 
 /* プッシュボタン */
-const int buttonPin = 32;               // 接続ピン
+// const int buttonPin = 32; // 接続ピン
 
 /* タイマー制御用 */
-Ticker  ticker;
-bool  bReadyTicker = false;         
-const int iIntervalTime = 1;           // 計測間隔（10秒）
+Ticker ticker;
+bool bReadyTicker = false;
+const int iIntervalTime = 1; // 計測間隔（10秒）
 
 /*********************< Callback classes and functions >**********************/
 // 接続・切断時コールバック
-class funcServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-        deviceConnected = true;
-    }
-    void onDisconnect(BLEServer* pServer) {
-        deviceConnected = false;
-    }
+class funcServerCallbacks : public BLEServerCallbacks
+{
+	void onConnect(BLEServer *pServer)
+	{
+		deviceConnected = true;
+	}
+	void onDisconnect(BLEServer *pServer)
+	{
+		deviceConnected = false;
+	}
 };
 
 //  タイマー割り込み関数  //
-static void kickRoutine() {
-    bReadyTicker = true;
+static void kickRoutine()
+{
+	bReadyTicker = true;
 }
 
 /*****************************************************************************
  *                          Predetermined Sequence                           *
  *****************************************************************************/
-void setup() {
-    // 初期化処理を行ってBLEデバイスを初期化する
-    doInitialize();
-    BLEDevice::init(DEVICE_NAME);
+void setup()
+{
+	Serial.begin(2400);
+	// 初期化処理を行ってBLEデバイスを初期化する
+	doInitialize();
+	BLEDevice::init(DEVICE_NAME);
 
-    // Serverオブジェクトを作成してコールバックを設定する
-    BLEServer *pServer = BLEDevice::createServer();
-    pServer->setCallbacks(new funcServerCallbacks());
+	// Serverオブジェクトを作成してコールバックを設定する
+	BLEServer *pServer = BLEDevice::createServer();
+	pServer->setCallbacks(new funcServerCallbacks());
 
-    // Serviceオブジェクトを作成して準備処理を実行する
-    BLEService *pService = pServer->createService(SERVICE_UUID);
-    doPrepare(pService);
+	// Serviceオブジェクトを作成して準備処理を実行する
+	BLEService *pService = pServer->createService(SERVICE_UUID);
+	doPrepare(pService);
 
-    // サービスを開始して、SERVICE_UUIDでアドバタイジングを開始する
-    pService->start();
-    BLEAdvertising *pAdvertising = pServer->getAdvertising();
-    pAdvertising->addServiceUUID(SERVICE_UUID);
-    pAdvertising->start();
-    // タイマー割り込みを開始する
-    ticker.attach(iIntervalTime, kickRoutine);
-    Serial.println("Waiting to connect ...");
+	// サービスを開始して、SERVICE_UUIDでアドバタイジングを開始する
+	pService->start();
+	BLEAdvertising *pAdvertising = pServer->getAdvertising();
+	pAdvertising->addServiceUUID(SERVICE_UUID);
+	pAdvertising->start();
+	// タイマー割り込みを開始する
+	ticker.attach(iIntervalTime, kickRoutine);
+	// Serial.println("Waiting to connect ...");
 }
 
-void loop() {
-    // 接続が確立されていて異常でなければ
-    if (deviceConnected && !bAbnormal) {
-        // タイマー割り込みによって主処理を実行する
-        if (bReadyTicker) {
-            doMainProcess();
-            bReadyTicker = false;
-        }
-        
-    }
+void loop()
+{
+	// 接続が確立されていて異常でなければ
+	if (deviceConnected && !bAbnormal)
+	{
+		// タイマー割り込みによって主処理を実行する
+		if (bReadyTicker)
+		{
+			doMainProcess();
+			bReadyTicker = false;
+		}
+	}
 }
 
 /*  初期化処理  */
-void doInitialize() {
-    Serial.begin(SPI_SPEED);
-    pinMode(buttonPin, INPUT);
-    pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, HIGH);
+void doInitialize()
+{
+	// Serial.begin(SPI_SPEED);
+	// pinMode(buttonPin, INPUT);
+	// pinMode(ledPin, OUTPUT);
+	// digitalWrite(ledPin, HIGH);
 }
 
 /*  準備処理  */
-void doPrepare(BLEService *pService) {
-    // Notify用のキャラクタリスティックを作成する
-    pCharacteristicTX = pService->createCharacteristic(
-                      CHARACTERISTIC_UUID_TX,
-                      BLECharacteristic::PROPERTY_NOTIFY
-                    );
-    pCharacteristicTX->addDescriptor(new BLE2902());
+void doPrepare(BLEService *pService)
+{
+	// Notify用のキャラクタリスティックを作成する
+	pCharacteristicTX = pService->createCharacteristic(
+		CHARACTERISTIC_UUID_TX,
+		BLECharacteristic::PROPERTY_NOTIFY);
+	pCharacteristicTX->addDescriptor(new BLE2902());
 }
 
 /*  主処理ロジック  */
-void doMainProcess() {
-    // ボタンを読み取る
-    int c = digitalRead(buttonPin);
+void doMainProcess()
+{
+	// ボタンを読み取る
+	while (Serial.available() > 0)
+	{
 
-    // 計測失敗なら再試行させる
-    if (isnan(c)) {
-        Serial.println("Failed to read sensor!");
-        return;
-    }
-    // 構造体に値を設定して送信する
-    data.buttonState = c;
-    pCharacteristicTX->setValue((uint8_t*)&data, sizeof(tmpData));
-    pCharacteristicTX->notify();
-    // シリアルモニターに表示する
-    
-    Serial.print("Send data: ");    Serial.println(c);
+		sensorData = Serial.read();
+		if (sensorData == 170)
+		{
+			z = 0;
+			incomeByte[z] = sensorData;
+		}
+		else
+		{
+			z++;
+			incomeByte[z] = sensorData;
+		}
+		if (z == 6)
+		{
+			sum = incomeByte[1] + incomeByte[2] + incomeByte[3] + incomeByte[4];
+
+			if (incomeByte[5] == sum && incomeByte[6] == 255)
+			{
+
+				// Serial.print("Data OK! |");
+				for (int k = 0; k < 7; k++)
+				{
+					Serial.print(incomeByte[k]);
+					Serial.print("|");
+				}
+
+				Serial.print(" Vo=");
+				float vo = (incomeByte[1] * 256.0 + incomeByte[2]) / 1024.0 * 5.00;
+				Serial.print(vo, 3);
+				Serial.print("v | ");
+				float c = vo * 700;
+
+				Serial.print(" PM2.5 = ");
+				Serial.print(c, 2);
+				Serial.print("ug/m3 ");
+				Serial.println();
+				data.pmData = (double)c;
+			}
+			else
+			{
+				z = 0;
+				Serial.flush();
+				sensorData = 0;
+				for (int m = 0; m < 7; m++)
+				{
+					incomeByte[m] = 0;
+				}
+			}
+			z = 0;
+		}
+	}
+
+	// 計測失敗なら再試行させる
+	// if (isnan(c)) {
+	//     Serial.println("Failed to read sensor!");
+	//     return;
+	// }
+	// 構造体に値を設定して送信する
+	pCharacteristicTX->setValue((uint8_t *)&data, sizeof(tmpData));
+	pCharacteristicTX->notify();
+	// シリアルモニターに表示する
+
+	// Serial.print("Send data: ");
+	// Serial.println(data.pmData);
 }
